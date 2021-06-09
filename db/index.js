c36cc47c4398258db638aedfec7c9d90356e9ee9
Jeffery <jefferyhauckjr@gmail.com>
2021-06-09 16:09:26 -0500

    cleaner code

--- db/index.js
+++ db/index.js
@@ -46,7 +46,7 @@ async function getLinksById(linkId) {
     if (!link) {
       throw {
         name: "ListNotFoundError",
-        message: "Could not find a lint with that linkId",
+        message: "Could not find a link with that linkId",
       };
     }
     const { rows: tags } = await client.query(
@@ -139,20 +139,7 @@ async function createTags(tagsList) {
   }
 }
 
-async function gettagsById(id) {
-  try {
-    const {
-      rows: [tags],
-    } = await client.query(`
-      SELECT *
-      FROM tags
-      WHERE id=${id}
-    `);
-    return tags;
-  } catch (error) {
-    throw error;
-  }
-}
+
 
 //links_Tags
 async function createLinkTags(linkId, tagId) {
@@ -184,88 +171,17 @@ async function addTagsToLinks(linkId, tagList) {
 
 //links
 
-async function createLinks({url, comment, date_shared, click_count, tags = []}) {
-  try {
-    const { rows: [ links ] } = await client.query(`
-      INSERT INTO links(url, comment, date_shared, click_count)
-      VALUES ($1, $2, $3, $4)
-      RETURNING *
-      `, [url, comment, date_shared, click_count]);
-
-      const tagsList = await createTags(tags)
-
-    return await  addTagsToLinks(links.id, tagsList);
-  } catch (error) {
-    throw error; 
-  }
-}
 
 
-async function getLinksById(linkId) {
-  try {
-    const {
-      rows: [link],
-    } = await client.query(
-      `
-      SELECT *
-      FROM links
-      WHERE id=$1;
-    `,
-      [linkId]
-    );
-    if (!link) {
-      throw {
-        name: "ListNotFoundError",
-        message: "Could not find a lint with that linkId"
-      };
-    }
-    const { rows: tags } = await client.query(
-      `
-      SELECT tags.*
-      FROM tags
-      JOIN link_tags ON tags.id=link_tags."tagId"
-      WHERE link_tags."linkId"=$1;
-    `,
-      [linkId]
-    );
 
-    link.tags = tags;
-    
 
-    return link;
-  } catch (error) {
-    throw error;
-  }
-}
 
 //url
 
-async function getLinksByUrl(url) {
-  try {
-    const { rows:[links] } = await client.query(`
-    SELECT *
-    FROM links
-    WHERE url=${url}
-    `)
-    return links
-  } catch(error) {
-    throw error
-  }
-}
+
 //tags
 
-async function getLinksByTags(tags) {
-  try {
-     const { rows: [links] } = await client.query(`
-     SELECT *
-     FROM links
-     WHERE tags=${tags}
-     `)
-     return links
-  } catch(error) {
-    throw error
-  }
-}
+
 
 //clickcount
 
@@ -289,7 +205,7 @@ async function getAllLinks() {
       FROM links
     `)
 
-    const allLinks = await Promise.all(links.map((link) => getLinksById(post.id)))
+    const allLinks = await Promise.all(links.map((link) => getLinksById(link.id)))
     return allLinks
   } catch(error) {
     throw error
@@ -300,31 +216,6 @@ async function getAllLinks() {
 
 //tags
 
-
-async function createTags(tagsList) {
-  if (tagsList.length === 0) {
-    return;
-  }
-  const insertValues = tagsList.map((_, index) => `$${index + 1}`).join("), (");
-  const selectValues = tagsList.map((_, index) => `$${index + 1}`).join(", ");
-  try {
-    await client.query(
-      `INSERT INTO tags(name)
-      VALUES (${insertValues})
-      ON CONFLICT (name) DO NOTHING;`,
-      tagsList
-    );
-    const { rows } = await client.query(
-      `SELECT * FROM tags
-      WHERE name
-      IN (${selectValues});`,
-      tagsList
-    );
-    return rows;
-  } catch (error) {
-    throw error;
-  }
-}
 
 
 async function getTagsById(id) {
@@ -341,35 +232,6 @@ async function getTagsById(id) {
 }
 
 
-//links_Tags
-
-async function createLinkTags(linkId, tagsId) {
-  try {
-     await client.query(`
-      INSERT INTO link_tags("linkId", "tagsId")
-      VALUES ($1, $2)
-      RETURNING *
-      `, [linkId, tagsId]);
-    
-  } catch (error) {
-    throw error; 
-  }
-}
-
-async function addTagsToLinks(linkId, tagsList) {
-  try {
-    const createLinkTagPromises = tagsList.map((tag) =>
-      createLinkTags(linkId, tag.id)
-    );
-
-    await Promise.all(createLinkTagPromises);
-
-    return await getLinksById(linkId);
-  } catch (error) {
-    throw error;
-  }
-}
-
 
 
 // export
@@ -379,7 +241,7 @@ module.exports = {
   createTags,
   createLinkTags,
   getLinksById,
-  gettagsById,
+  getTagsById,
   getLinksByClickCount,
   getLinksByUrl,
   getLinksByTags,
